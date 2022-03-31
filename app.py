1d71a6c447bb20e6f5a43363f3dcf4cd4ee8dc06
ZiadAlnagar189714 <96744809+ZiadAlnagar189714@users.noreply.github.com>
2022-03-31 23:09:58 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -9,14 +9,17 @@ from sklearn.cluster import AgglomerativeClustering
 
 colnames = ['surgery', 'age', 'hospital number', 'rectal temperature', 'pulse', 'respiratory rate', 'temperature of extremities', 'peripheral pulse', 'mucous membranes', 'capillary refill time', 'pain', 'peristalsis', 'abdominal distension', 'nasogastric tube',
             'nasogastric reflux', 'nasogastric reflux PH', 'rectal examination', 'abdomen', 'packed cell volume', 'total protein', 'abdominocentesis appearance', 'abdomcentesis total protein', 'outcome', 'surgical lesion', 'lesion type1', 'lesion type2', 'lesion type3', 'pathology data']
-catagorical_columns = []
 numerical_columns = ['rectal temperature', 'pulse', 'respiratory rate',
                      'nasogastric reflux', 'packed cell volume', 'total protein', 'abdomcentesis total protein']
+# Read dataset file 10 20, 
 df = pd.read_csv("data\horse-colic.data",
-                 sep=' ', names=colnames, header=None)
+                 sep=' ', names = colnames, header=None)
 df.columns.names = ['id']
 
+# print(df)
 # df.drop(['Column1', 'Column2', 'Column3', 'Column4'], axis=1, inplace=True)
+
+# Pre-processing
 
 # Replace missing values with the most frequant value
 complete_df = df.copy()
@@ -24,6 +27,7 @@ for col in complete_df.columns:
     complete_df[col] = complete_df[col].replace('?', np.nan)
     complete_df[col] = complete_df[col].fillna(complete_df[col].mode()[0])
 
+# Normalization
 normalized_df = complete_df.copy()
 to_norm = normalized_df.loc[:, numerical_columns]
 # col_normalized = (norm-norm.min()) / (norm.max() - norm.min())
@@ -52,9 +56,11 @@ for col in colnames:
     plt.scatter(refined_df[col], refined_df['clusters'],
                 c=refined_df['clusters'], cmap='rainbow')
     n += 1
-
-# Hieratical
+    
+    # Hieratical
 refined_df_2 = normalized_df.copy()
+
+# Draw Dendrogram
 plt.figure(figsize=(16, 16))
 plt.title("Horse Colic Dendograms")
 dend = shc.dendrogram(shc.linkage(
@@ -63,7 +69,7 @@ plt.ylim(0, 65000)
 
 Aglo = AgglomerativeClustering(
     n_clusters=3, affinity='euclidean', linkage='ward')
-clusters = cluster.fit_predict(refined_df_2)
+clusters = Aglo.fit_predict(refined_df_2)
 refined_df_2['clusters'] = pd.Series(clusters)
 
 n = 0
